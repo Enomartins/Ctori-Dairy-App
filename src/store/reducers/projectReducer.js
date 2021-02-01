cb249bc545d235ef26687b800de4d52bfbb09728
Enomartins <enowebservices@gmail.com>
2021-02-01 19:09:45 +0100

    local materialize setup added, and issue with key fixed

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -2,7 +2,7 @@ const initState = {
     projects: [
         {id:1, title: "first", content: "content of the first"},
         {id:2, title: "second", content: "content of the second"},
-        {id:2, title: "third", content: "content of the third"}
+        {id:3, title: "third", content: "content of the third"}
     ]
 }
 
